acfdd18aeccb7214ae04c85306f4e914da4e9ec8
ScotuzziJr <scotuzzijunior@gmail.com>
2023-10-06 18:40:22 -0300

    feat: creating a css base file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { createTheme, ThemeProvider } from '@mui/material';
+
+import HeaderLogin from "./components/HeaderLogin";
 import Login from "./components/Login";
+
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Poppins',
+      'sans-serif',
+    ].join(','),
+  },});
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <HeaderLogin />
+        <Login />
+      </div>
+    </ThemeProvider>
   );
 }
 
