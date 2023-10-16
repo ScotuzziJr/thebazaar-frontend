d4be90a55e712662866e0acc4220d19c463e67b3
ScotuzziJr <scotuzzijunior@gmail.com>
2023-10-16 14:56:58 -0300

    feat: creating the register page and making API calls

---
+++ src/components/Register.jsx
@@ -0,0 +1,104 @@
+import { Box, Button, TextField, Typography } from '@mui/material'
+import React, { useState } from 'react'
+
+import HeaderLogin from './HeaderLogin';
+
+const Register = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+
+
+    const handleRegistration = async () => {
+        const new_user = await fetch("http://localhost:8000/api/users/create", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                "username": username,
+                "email": email,
+                "password": password
+            })
+        })
+        .then((res) => {
+            if(res.status === 201) {
+                alert("Successfull registration");
+            } else {
+                alert("Registration failed");
+            }
+
+            return res.json();
+        })
+    }
+
+    return (
+        <Box>
+            <HeaderLogin />
+
+            <form>
+                <Box 
+                    display={"flex"} 
+                    flexDirection={"column"}
+                    alignItems={"center"} 
+                    justifyContent={"center"}
+                    maxWidth={400}
+                    margin={"auto"}
+                    marginTop={10}
+                    padding={5}
+                    borderRadius={5}
+                    boxShadow={"5px 5px 10px #ccc"}
+                    sx={{
+                        ":hover": {
+                            boxShadow: "10px 10px 20px #ccc"
+                        }
+                }}
+                >
+                    <Typography
+                        variant='h4'
+                        padding={3}
+                    >
+                        Registration
+                    </Typography>
+
+                    <TextField 
+                        type="text"
+                        variant="outlined" 
+                        placeholder="Username"
+                        margin="normal"
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+
+                    <TextField 
+                        type="text"
+                        variant="outlined" 
+                        placeholder="Email"
+                        margin="normal"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    
+                    <TextField 
+                        type="password"
+                        variant="outlined" 
+                        placeholder="Password"
+                        margin="normal"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    
+                    <Button
+                        onClick={handleRegistration}
+                        variant="contained"
+                        sx={{
+                            margin: 3,
+                            borderRadius: 3
+                        }}
+                    >
+                        Submit
+                    </Button>
+                </Box>
+            </form>
+        </Box>
+    );
+}
+
+export default Register;
